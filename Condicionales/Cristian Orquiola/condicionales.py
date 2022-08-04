17f49b27b4b725a5832613e106a9b9f0dfc204e8
Arbusta <cristian.orquiola@arbusta.net>
2022-08-04 18:17:06 -0300

    entregue trabajo

---
+++ Condicionales/Cristian Orquiola/condicionales.py
@@ -0,0 +1,59 @@
+# 1- Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+# edad = int(input("¿cuantos años tenes?: "))
+# mayor = 18
+# if (edad>=mayor):
+#     print("es mayor de edad")
+# else:
+#     print("no es mayor de edad")
+
+# 2.- Escribir un programa que pida al usuario dos números y muestre por pantalla su división. Si el divisor es cero el programa debe mostrar un error.
+
+
+# num1 = int(input("ingrese el primer numero para dividir: "))
+# num2 = int (input("ingrese el segundo numero: "))
+# if num2 == 0:
+#     print("error")
+# else:    
+#     print (num1/num2)
+   
+# 3.- Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar
+
+# pedir = int(input("escribe un numero te dire si es par o impar: "))
+# if pedir%2==0: 
+#     print("es par")
+
+# elif pedir%2 !=0: 
+#     print("no es par")
+    
+# else:
+#     print("ninguno es par")     
+
+# 4.- La pizzería Bella Napoli ofrece pizzas vegetarianas y no vegetarianas a sus clientes. #
+# Los ingredientes para cada tipo de pizza aparecen a continuación.
+
+# Ingredientes vegetarianos: Pimiento y tofu.
+# Ingredientes no vegetarianos: Peperoni, Jamón y Salmón.
+
+# Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, y en #
+# función de su respuesta le muestre un menú con los ingredientes disponibles para que #
+# elija. Solo se puede eligir un ingrediente además de la mozzarella y el tomate que están #
+# en todas la pizzas. Al final se debe mostrar por pantalla si la pizza elegida es #
+# vegetariana o no y todos los ingredientes que lleva.
+
+# cliente = str(input("bienvenido, decea una pizza vegetariana o no vegetariana: "))
+# if cliente == "vegetariana": 
+#              condi= input("quiere pimiento o tofu")
+#              if condi == "pimienta":
+#               print("usted eligio una pizza vegetariana con tomate muzarella y pimienta")
+#              elif condi == "tofu":
+#               print("usted eligio una pizza vegetariana con tomate muzarella y tofu")
+# elif cliente == "no vegetariana":
+#               condi2= input("quiere peperoni, jamon o salmon?: ")
+#               if condi2== "peperoni":
+#                print("usted eligio una pizza con tomate muzarella y peperoni")
+#               elif condi2 == "jamon":
+#                print("usted eligio una pizza con tomate muzarella y jamon")
+#               elif condi2 == "salmon":
+#                print("usted eligio una pizza con tomate muzarella y salmon")
+               
