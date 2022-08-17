c25d216ad4ed2e2e629fdb6fbfbd69de97065bb2
Arbusta <morinigo.matias@arbusta.net>
2022-08-17 10:03:18 -0300

    Evaluacion

--- Condicionales/Matias Morinigo/EjerciciosPrac.py
+++ Condicionales/Matias Morinigo/EjerciciosPrac.py
@@ -1,42 +1,71 @@
 # Solicitar al usuario que ingrese su dirección email. Imprimir un mensaje indicando si la dirección es válida o no, valiéndose de una función para 
 # decidirlo. Una dirección se considerará válida si contiene el símbolo "@"
 
-pregunta = input("Ingrese su direccion de gmail: ")
-arr = "@"
-
-def num(arr):
-    for i in pregunta:
-        if i == arr:
-            return "El mail es valido"
-        else:
-            print(arr in pregunta, "La direccion no es valida")
-mail = num(arr)
-print(mail)
+# pregunta = input("Ingrese su direccion de gmail: ")
+# arr = "@"
+# def num(pregunta):                          
+#     for i in pregunta:
+#         if i == arr:
+#             return "El mail es valido"
+#     return "La direccion no es valida"
+# mail = num(pregunta)
+# print(mail)
 
 
 
 # 2. Solicitar números al usuario hasta que ingrese el cero. Por cada uno, mostrar la suma de sus dígitos (utilizando una función que realice dicha suma)
 
+# def suma(n):
+#     sum = [int(x) for x in str(n)]
+#     b = 0
+#     for i in sum:
+#         b += i 
+#     return b 
 # numero = int(input("Ingrese un numero: "))
-# numero2 = int(input("Ingrese un numero: "))
-# # def sum(numero, numero2):
-# while numero + numero2:
-#     numero + numero2
-#     numero == 0
-#     break
+# while numero != 0:
+#     print(suma(numero))
+#     numero = int(input("Ingrese un numero: "))
+# print("Finaliza")
 
 
+# def  sumadigitos (a) : # defino funcion (a) puede val
+#     digitos =[int (x) for x in str(a)]   #va a leer el elemento
+#     acumulador = 0   #declaro el acumulador ene su posicion 
 
+#     for i in digitos:  
+  
 
-    
-#     suma2 = numero + numero2
-#     return suma2
-# suma = numero + numero2
-#print(suma)
+#         acumulador+=i 
+#     return acumulador
+# numero = input("ingrese un numero")
+# while numero !="0": # cero como string 
+#     print (sumadigitos(numero))
+#     numero = input("ingrese un numero")
 
+# print ("bucle finalizado")
 
+# while numero != x:
+#     if numero > x:
+#         numero = int(input("Ingrese un numero: "))                       # Sobrescribe la variable
+#     if numero == x:
+#         break
+# print("Bucle finalizado")
 
 
 
 # 3. Solicitar números al usuario hasta que ingrese el cero. Por cada uno, mostrar la suma de sus dígitos. Al finalizar, mostrar la sumatoria 
 # de todos los números ingresados y la suma de sus dígitos. Reutilizar la misma función realizada en el ejercicio 2,
+
+def suma(n):
+    sum = [int(x) for x in str(n)]
+    b = 0
+    for i in sum:
+        b += i 
+    return b 
+numero = int(input("Ingrese un numero: "))
+while numero != 0:
+    print(suma(numero))
+    numero = int(input("Ingrese un numero: "))
+print("Finaliza")
+
+
---
+++ Evaluacion 16-8/Matías Morinigo/Evaluación.py
@@ -0,0 +1,34 @@
+# 1. Crea una función que dados dos números mostrará todos los números que hay entre ellos
+
+# num1 = int(input("Ingrese un numero: "))
+# num2 = int(input("Ingrese un numero: "))
+
+# def num(num1, num2):
+#     for i in (range(num1, num2 + 1)):
+#         print(i)
+# print(num(num1, num2))
+
+
+
+# 2. Escribir una función que, dado un número de DNI, retorne True si el número es válido y False si no lo es. Para que un número de 
+# DNI sea válido debe tener entre 7 y 8 dígitos.
+
+numDni = int(input("Ingrese su numero de DNI: "))
+
+def dni(numDni):                                 # No se me ocurre como hacer que tome si es valido o no
+    if numDni 
+        return True
+    else:
+        return False
+print(dni(numDni))
+
+
+
+# 3. Crea una funcion que retorne tu nombre y color favorito.
+
+# nombre = input("Ingrese su nombre: ")
+# colorFav = input("Ingrese su color favorito: ")
+
+# def col(nombre, colorFav):
+#         return colorFav, nombre
+# print(col(nombre, colorFav))
